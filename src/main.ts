c052499dc0d5fe18ff1c8893a4cd8c375908322a
BagusAK95 <bagus@kata.ai>
2021-10-12 22:21:59 +0700

    remoce validation option

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,7 @@ import { HttpExceptionFilter } from './exception/exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-    disableErrorMessages: false,
-  }));
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
