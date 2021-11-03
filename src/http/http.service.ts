48d7276c0d9ab45ca4ddb5b54bbc55bfce005e06
BagusAK95 <bagus@kata.ai>
2021-11-03 10:11:17 +0700

    set response

--- src/http/http.service.ts
+++ src/http/http.service.ts
@@ -14,31 +14,54 @@ export class HttpService {
     this.instance = axios.default.create(this.config);
   }
 
-  async request(config: axios.AxiosRequestConfig) {
+  async request(
+    config: axios.AxiosRequestConfig,
+  ): Promise<axios.AxiosResponse> {
     return await this.instance.request(config);
   }
 
-  async get(url: string, config?: axios.AxiosRequestConfig): Promise<axios.AxiosResponse> {
-    return await this.instance.get(url, config)
+  async get(
+    url: string,
+    config?: axios.AxiosRequestConfig,
+  ): Promise<axios.AxiosResponse> {
+    return await this.instance.get(url, config);
   }
 
-  async delete(url: string, config?: axios.AxiosRequestConfig) {
+  async delete(
+    url: string,
+    config?: axios.AxiosRequestConfig,
+  ): Promise<axios.AxiosResponse> {
     return await this.instance.delete(url, config);
   }
 
-  async head(url: string, config?: axios.AxiosRequestConfig) {
+  async head(
+    url: string,
+    config?: axios.AxiosRequestConfig,
+  ): Promise<axios.AxiosResponse> {
     return await this.instance.delete(url, config);
   }
 
-  async post(url: string, data?: any, config?: axios.AxiosRequestConfig) {
+  async post(
+    url: string,
+    data?: any,
+    config?: axios.AxiosRequestConfig,
+  ): Promise<axios.AxiosResponse> {
     return await this.instance.post(url, data, config);
   }
 
-  async put(url: string, data?: any, config?: axios.AxiosRequestConfig) {
+  async put(
+    url: string,
+    data?: any,
+    config?: axios.AxiosRequestConfig,
+  ): Promise<axios.AxiosResponse> {
     return await this.instance.put(url, data, config);
   }
 
-  async patch(url: string, data?: any, config?: axios.AxiosRequestConfig) {
+  async patch(
+    url: string,
+    data?: any,
+    config?: axios.AxiosRequestConfig,
+  ): Promise<axios.AxiosResponse> {
     return await this.instance.patch(url, data, config);
   }
 }
